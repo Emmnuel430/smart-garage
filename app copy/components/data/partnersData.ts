4c78933d37b62e2abcd4b561e2621951632c7ca2
joel.daho <joel.daho@asnumeric.com>
2025-03-04 14:02:42 +0000

    Quatrieme commit

--- app copy/components/data/partnersData.ts
+++
@@ -1,5 +0,0 @@
-export const partners = [
-  { imgSrc: "/picture/kagnan.png", alt: "Garage Kagnan" },
-  { imgSrc: "/picture/ciera.jpeg", alt: "Ciera" },
-  { imgSrc: "/picture/lk-automotive.PNG", alt: "LK Automotive" },
-];
